93eb2b55c02c8973ead096aae122856de3a5295c
Mohanned Binmiskeen <mohn93@gmail.com>
2020-08-06 13:52:17 +0200

    fix

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ exports.root = __dirname;
 exports.init = function (N) { require('./lib/autoload.js')(N); };
 
 
-if (!module.parent) {
-  require('./lib/system/runner').bootstrap(exports);
-}
+// if (!module.parent) {
+require('./lib/system/runner').bootstrap(exports);
+// }
