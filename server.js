d78741a7a97bc58f9a23edfff0b017957f76eb8f
Mohanned Binmiskeen <mohn93@gmail.com>
2020-08-08 12:24:11 +0200

    try

--- server.js
+++ server.js
@@ -3,11 +3,11 @@
 
 'use strict';
 
-
+console.log('Init app');
 exports.root = __dirname;
 exports.init = function (N) { require('./lib/autoload.js')(N); };
 
 
-// if (!module.parent) {
-require('./lib/system/runner').bootstrap(exports);
-// }
+if (!module.parent) {
+  require('./lib/system/runner').bootstrap(exports);
+}
